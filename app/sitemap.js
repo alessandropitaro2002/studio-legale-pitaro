3380d6ac94fc3767854dbf8f31472768d3ba6f3d
alessandropitaro2002 <alessandropitaro2002@gmail.com>
2026-05-12 11:10:42 +0200

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -15,6 +15,12 @@ export default function sitemap() {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/attivita-professionale`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/privacy-policy`,
       lastModified: new Date(),
       changeFrequency: "yearly",
